427b417f5806afb1e847ab28609e3fc4778c9604
Mario Seidl <mario.s-@live.de>
2020-09-29 11:34:40 +0200

    created component FlyerCustomizer

---
+++ src/components/FlyerCustomizer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <label>
+      <input v-model="title">
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlyerCustomizer",
+  data: function () {
+    return {
+      title: ""
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
