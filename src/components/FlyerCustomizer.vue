90ac6143726eb722cc13370ae60e44c3e162646e
Mario Seidl <mario.s-@live.de>
2020-09-29 12:14:10 +0200

    test jsPDF

--- src/components/FlyerCustomizer.vue
+++ src/components/FlyerCustomizer.vue
@@ -7,12 +7,23 @@
 </template>
 
 <script>
+import jsPDF from 'jspdf'
+
 export default {
   name: "FlyerCustomizer",
   data: function () {
     return {
-      title: ""
+      company: "",
+      text: ""
     };
+  },
+  methods:{
+    createPDF: function (){
+      let documentName = 'Flyer';
+      let document = new jsPDF();
+      document.text("", 10, 10)
+      document.save(documentName + '.pdf')
+    }
   }
 }
 </script>
