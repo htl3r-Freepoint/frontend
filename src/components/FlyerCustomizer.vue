39a382a7792012098f628424efd762d44ee0cc35
Mario Seidl <mario.s-@live.de>
2020-10-06 12:18:15 +0200

    Ich struggle und hab keine Ahnung

--- src/components/FlyerCustomizer.vue
+++ src/components/FlyerCustomizer.vue
@@ -3,26 +3,31 @@
     <label>
       <input v-model="title">
     </label>
+    <button @click="createPDF">create PDF</button>
+
+    <div id="qrcode"></div>
+    <qrcode value="Hello, World!" :options="{ width: 200 }"></qrcode>
   </div>
 </template>
 
 <script>
-import jsPDF from 'jspdf'
+import JsPDF from 'jspdf'
 
 export default {
   name: "FlyerCustomizer",
   data: function () {
     return {
       company: "",
-      text: ""
+      text: "",
+      qrCodeURL: "diplomarbeit.freepoint.at"
     };
   },
-  methods:{
-    createPDF: function (){
-      let documentName = 'Flyer';
-      let document = new jsPDF();
-      document.text("", 10, 10)
-      document.save(documentName + '.pdf')
+  methods: {
+    createPDF: function () {
+      let documentName = 'Flyer'
+      let doc = new JsPDF()
+      doc.addImage(null, 15, 40, 180, 180)
+      doc.save(documentName + '.pdf')
     }
   }
 }
