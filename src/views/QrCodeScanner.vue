c002aabcd68bffb83b95a3dad8b5c123e6c5c87a
Mario Seidl <mario.s-@live.de>
2020-10-22 14:52:44 +0200

    Qrcode fertig

--- src/views/QrCodeScanner.vue
+++ src/views/QrCodeScanner.vue
@@ -1,36 +1,58 @@
 <template>
   <div>
     <h1>Qr-Code Scanner</h1>
-    <p class="error">{{ error }}</p>
 
-    <p class="decode-result">Last result: <b>{{ result }}</b></p>
-    <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+    <qrcode-stream @decode="onDecode" @init="checkCamera"></qrcode-stream>
+    <div v-if="error">
+      <qrcode-drop-zone @decode="onDecode"></qrcode-drop-zone>
+      <qrcode-capture @decode="onDecode"></qrcode-capture>
+    </div>
   </div>
 </template>
 
 <script>
-import { QrcodeStream } from 'vue-qrcode-reader'
+import {QrcodeStream, QrcodeDropZone, QrcodeCapture} from 'vue-qrcode-reader'
+import Axios from "axios";
 
 export default {
   name: "QrCodeScannerView",
-  components: {QrcodeStream},
+  // eslint-disable-next-line vue/no-unused-components
+  components: {QrcodeStream, QrcodeDropZone, QrcodeCapture},
   data() {
     return {
-      result: '',
+      regex: "_?R\\d-AT\\d_(.+)_(.+)_(\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2})_(\\d+,\\d{2})_(\\d+,\\d{2})_(\\d+,\\d{2})_(\\d+,\\d{2})_(\\d+,\\d{2})_(.+)={1,2}_(.+)==",
+      url: 'http://freepoint.at',
+      options: {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json;charset=UTF-8'
+        },
+      },
       error: ''
     }
   },
   methods: {
-    onDecode(result) {
-      this.result = result
-    },
 
-    async onInit(promise) {
+    onDecode(result) {
+      if (this.regex.exec(result)) {
+        Axios.post(this.url, {
+          params: {
+            data: result
+          }
+        }, this.headers)
+      }else{
+        console.error("Qrcode is invalid");
+      }
+    },
+    async checkCamera(promise) {
       try {
         await promise
+        console.log("Qrcode-Scanner loaded")
       } catch (error) {
+        console.error(error);
         if (error.name === 'NotAllowedError') {
-          this.error = "ERROR: you need to grant camera access permisson"
+          this.error = "ERROR: you need to grant camera access permission"
         } else if (error.name === 'NotFoundError') {
           this.error = "ERROR: no camera on this device"
         } else if (error.name === 'NotSupportedError') {
@@ -42,9 +64,12 @@ export default {
         } else if (error.name === 'StreamApiNotSupportedError') {
           this.error = "ERROR: Stream API is not supported in this browser"
         }
+        console.error(this.error)
       }
     }
+
   }
+
 }
 </script>
 
