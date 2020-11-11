1a563b0d632c754c10d6110137cc92db4122c995
Kristian <5115@htl.rennweg.at>
2020-11-11 23:14:16 +0100

    simple card

--- src/components/RabattMenu.vue
+++ src/components/RabattMenu.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="section">
-    <div class="row">
-      <div class="col; card">
-        One of three columns
-      </div>
-      <div class="col; card">
-        One of three columns
-      </div>
-      <div class="col; card">
-        One of three columns
+  <div class="container">
+    <div id="head row">
+      <div class="col-6"></div>
+      <div class="col-6">Flame</div>
+    </div>
+    <div class="row" id="coupon">
+      <div class="col-md-4">
+        <div class="card rounded">
+          <div class="card-body text-center m-auto">
+            <h5>Cheeseburger</h5>
+          </div>
+          <span class="card-edit"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil"
+                                       fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd"
+        d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+</svg></span>
+          <span class="card-delete"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash"
+                                         fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path
+      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fill-rule="evenodd"
+        d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></span>
+          <div class="row">
+            <div class="col-5 card-body text-left">
+              <h3>Cheeseburger</h3>
+            </div>
+            <div class="col-7 card-body text-right">
+              <span class="card-points">25FP</span>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   </div>
@@ -16,10 +39,11 @@
 
 <script>
 import Axios from 'axios'
+
 export default {
   name: "RabattMenu",
   data() {
-    return{
+    return {
       url: 'http://freepoint.at',
       user: '1',
       store: '1',
@@ -32,8 +56,8 @@ export default {
       }
     }
   },
-  methods:{
-    getData(){
+  methods: {
+    getData() {
       Axios.get(this.url, {
         params: {
           user: this.user,
@@ -47,27 +71,77 @@ export default {
 
 <style scoped>
 
-.section {
-  margin: auto;
-  background: grey;
-  opacity: .5;
-  border-radius: 3px;
-  margin-top: 50px;
-  padding: 10px;
+
+/* Category Ads */
+
+#coupon {
+  margin: 30px 0 30px 0;
+
 }
 
-.card {
-  width: 30%;
-  align-self: center;
-  background: whitesmoke;
-  padding: 5px;
-  border-radius: 3px;
-  margin: 10px 10px;
-  font-family: monospace;
-}
-
-.card.title {
+#coupon .card-picture {
+  position: absolute;
+  left: -10px;
+  top: -20px;
+  background: #f2d900;
   text-align: center;
+  border-radius: 30px 30px 30px 30px;
+  color: #000;
+  padding: 5px 10px;
+  font-size: 14px;
+
 }
+
+#coupon .card-edit {
+  position: absolute;
+  right: -10px;
+  top: -20px;
+  background: #ff4444;
+  border-radius: 50%;
+  text-align: center;
+  color: #fff;
+  width: 30px;
+  height: 30px;
+}
+
+#coupon .card-delete {
+  position: absolute;
+  right: -10px;
+  top: 30px;
+  background: #ff4444;
+  border-radius: 50%;
+  text-align: center;
+  color: #fff;
+  width: 30px;
+  height: 30px;
+}
+
+#coupon .card-points {
+  position: absolute;
+  right: 0px;
+  top: 40px;
+  border-radius: 50%;
+  text-align: center;
+  color: #10cdb7;
+  width: 30px;
+  height: 30px;
+}
+
+#coupon .card:hover {
+  background: #fff;
+  box-shadow: 12px 15px 20px 0px rgba(46, 61, 73, 0.15);
+  border-radius: 4px;
+  transition: all 0.3s ease;
+}
+
+#coupon .card-image-overlay {
+  font-size: 20px;
+}
+
+
+#coupon .card-image-overlay span {
+  display: inline-block;
+}
+
 
 </style>
