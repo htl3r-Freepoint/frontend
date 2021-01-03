d8da90cfb12bfa7b0532bffdc43f79a3f91a6e99
Kristian <5115@htl.rennweg.at>
2021-01-03 15:31:27 +0100

    farbe geändert

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #153E73;
   min-height: 100vh;
 }
 
