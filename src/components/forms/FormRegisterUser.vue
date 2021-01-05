66054bcd1e231f47fcc77f0f7844b13b2c5a72ad
Mario Seidl <mario.s-@live.de>
2021-01-05 12:53:09 +0100

    Changed API feature

--- src/components/forms/FormRegisterUser.vue
+++ src/components/forms/FormRegisterUser.vue
@@ -4,7 +4,7 @@
     <div class="col form-group">
       <div class="row">
         <label for="inputRegisterEmail" class="col-3 col-form-label">E-Mail:</label>
-        <input type="email" class="col form-control" id="inputRegisterEmail" v-model="this.email"
+        <input type="email" class="col form-control" id="inputRegisterEmail" v-model="email"
                placeholder="E-Mail..." required>
       </div>
       <div class="row">
@@ -15,29 +15,29 @@
     <div class="col form-group">
       <div class="row">
         <label for="inputRegisterPassword" class="col-3 col-form-label">Password:</label>
-        <input type="password" class="col form-control" id="inputRegisterPassword" v-model="this.password"
-               placeholder="Password..." required>
+        <input type="password" class="col form-control" id="inputRegisterPassword" v-model="password"
+               placeholder="Password..." autocomplete="new-password" required>
       </div>
     </div>
     <div class="col form-group">
       <div class="row">
         <label for="inputRegisterPasswordConfirm" class="col-3 col-form-label"></label>
-        <input type="password" class="col form-control" id="inputRegisterPasswordConfirm" v-model="this.passwordConfirm"
-               placeholder="Confirm Password..." required>
+        <input type="password" class="col form-control" id="inputRegisterPasswordConfirm" v-model="passwordConfirm"
+               placeholder="Confirm Password..." autocomplete="new-password" required>
       </div>
     </div>
     <div class="col form-group">
       <div class="row">
-        <label for="inputRegisterUsername" class="col-3 col-form-label">Name:</label>
-        <input type="text" class="col form-control" id="inputRegisterUsername" v-model="this.name"
-               placeholder="Name..." required>
+        <label for="inputRegisterUsername" class="col-3 col-form-label">Username:</label>
+        <input type="text" class="col form-control" id="inputRegisterUsername" v-model="name"
+               placeholder="Username..." autocomplete="username" required>
       </div>
     </div>
     <div class="col form-check">
       <div class="row">
         <div class="col-3"/>
         <div class="col">
-          <input type="checkbox" class="form-check-input" id="inputRegisterTOS" v-model="this.TOS" required>
+          <input type="checkbox" class="form-check-input" id="inputRegisterTOS" v-model="TOS" required>
           <label for="inputRegisterTOS">By checking this, you hereby agree to our
             <router-link to="/terms-and-service">Terms and Services</router-link>
           </label>
@@ -48,7 +48,7 @@
       <div class="row">
         <div class="col-3"/>
         <div class="col">
-          <button type="submit" class="btn btn-primary" @click="this.register()">Register</button>
+          <button type="button" class="btn btn-primary" v-on:click="register()">Register</button>
         </div>
       </div>
     </div>
@@ -68,7 +68,7 @@ export default {
       passwordConfirm: "",
       name: "",
       TOS: false,
-      url: 'http://freepoint.at/api/user.json',
+      url: 'https://freepoint.htl3r.com/api/RegisterUser.json',
       data: {
         params: {
           email: this.email,
@@ -84,21 +84,26 @@ export default {
   },
   methods: {
     register() {
-      if (this.email && this.password && this.password === this.passwordConfirm) {
-        Axios.post(this.url, this.data, this.headers)
-            .then()
-            .catch(function (error) {
-              if (error.response) {
-                console.log(error.response.data);
-                console.log(error.response.status);
-                console.log(error.response.headers);
-              } else if (error.request) {
-                console.log(error.request);
-              } else {
-                console.log('Error', error.message);
-              }
-              console.log(error.config);
-            })
+      if (this.email && this.password && this.passwordConfirm && this.TOS) {
+        if (this.password === this.passwordConfirm) {
+          Axios.post(this.url, this.data, this.headers)
+              .then()
+              .catch(function (error) {
+                if (error.response) {
+                  console.log(error.response.data);
+                  console.log(error.response.status);
+                  console.log(error.response.headers);
+                } else if (error.request) {
+                  console.log(error.request);
+                } else {
+                  console.log('Error', error.message);
+                }
+                console.log(error.config);
+              })
+        }else console.log("The repeated password must be equal")
+      } else {
+        console.log("Form incomplete")
+        console.log([this.email, this.password, this.passwordConfirm, this.TOS])
       }
     }
   }
