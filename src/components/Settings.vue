cd1d770740cc5c9d10c3e7aa6e59f8c948de9b15
Kristian <5115@htl.rennweg.at>
2021-02-01 14:20:01 +0100

    layout verändert

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="card container">
-    <div class="row settings-header">
-      <div class="col-1 text-left">
-        <router-link class="router-link" to="/settings/user">
-          <i class="fas fa-arrow-left fa-2x"></i>
+    <div class="row">
+      <h3 class="col-3 text-left settings-main-header">Einstellungen</h3>
+      <h4 class="col settings-sub-header">{{ title }}</h4>
+    </div>
+    <div class="row">
+      <div class="col-3 settings-links">
+        <router-link class="router-link" to="/settings/user/profile">
+          <div class="row settings-row">
+            <div class="col-1"><i class="far fa-user-circle settings-icon"></i></div>
+            <div class="col text-left">Profil bearbeiten</div>
+          </div>
+        </router-link>
+
+        <router-link class="router-link" to="/settings/user/info">
+          <div class="row settings-row">
+            <div class="col-1"><i class="fas fa-info settings-icon"></i></div>
+            <div class="col text-left">Info</div>
+          </div>
+        </router-link>
+        <router-link class="router-link" to="/settings/user/help">
+          <div class="row settings-row">
+            <div class="col-1"><i class="far fa-question-circle settings-icon"></i></div>
+            <div class="col text-left">Hilfe</div>
+          </div>
+        </router-link>
+        <router-link class="router-link" to="/settings/user/feedback">
+          <div class="row settings-row">
+            <div class="col-1"><i class="far fa-comment-dots settings-icon"></i></div>
+            <div class="col text-left">Feedback</div>
+          </div>
         </router-link>
       </div>
-      <h3 class="col text-left">{{ title }}</h3>
+      <div class="col">
+        <slot name="body"></slot>
+      </div>
     </div>
-    <slot name="body"></slot>
+
   </div>
 </template>
 
 <script>
 export default {
-name: "Settings",
+  name: "Settings",
   props: ['title']
 }
 </script>
@@ -26,7 +54,36 @@ name: "Settings",
   color: inherit;
 }
 
-.settings-header {
-  margin-bottom: 1em;
+.settings-row{
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
 }
+
+.settings-row:hover{
+  background: lightgrey;
+}
+
+.router-link-active{
+  font-weight: bold;
+  
+}
+
+.settings-links {
+  background: #F1F1F1;
+}
+
+.settings-main-header{
+  background: #F1F1F1;
+  padding-top: 1em;
+  padding-bottom: 0.5em;
+  margin-bottom: 0px;
+}
+
+.settings-sub-header {
+  background: white;
+  padding-top: 1em;
+  padding-bottom: 0.5em;
+}
+
+
 </style>
--- src/views/Info.vue
+++ src/views/Info.vue
@@ -2,6 +2,18 @@
   <div>
     <settings title="Info">
       <div slot="body">
+        <div class="text-left">Nutzungsbedingungen
+          FAQ:
+          Wie funktioniert die App?
+          Die Applikation "Name der Geschäftsapp" wurde mithilfe von FreePoint erstellt. FreePoint übernimmt die
+          Funktionalität als auch die Wartung dieser App. Das Geschäft "Name des Geschäftes" hat freie
+          Entscheidungsmacht
+          über die Rabatte und das Design.
+          Wie entstand FreePoint?
+          Freepoint entstand aus der Ideenschmiede vier junger Männer der HTL Rennweg Wien. Diese haben dieses Projekt
+          im
+          Rahmen ihrer Diplomarbeit umgesetzt. Nähere Informationen finden sie unter https://diplomarbeit.freepoint.at/
+        </div>
       </div>
     </settings>
   </div>
