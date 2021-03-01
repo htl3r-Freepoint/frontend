c8ee8fdc5fc546d039c97be38d97d16be0fee53f
Kristian <5115@htl.rennweg.at>
2021-03-01 23:15:12 +0100

    weitergemacht

--- src/views/company/settings/FlyerCustomizer.vue
+++ src/views/company/settings/FlyerCustomizer.vue
@@ -19,21 +19,21 @@
         <div class="col">
           <input type="radio" class="sr-only" name="layout" id="layout1" v-model="layout" value="0">
           <label for="layout1">
-            <img src="../../../assets/flyer_layout_preview/Flyer_Layout_1_Vorschau_Border.png" class="w-50">
+            <img src="../../../assets/flyer/Flyer-Layout-1-Vorschau.png" class="w-75">
           </label>
         </div>
 
         <div class="col">
           <input type="radio" class="sr-only" name="layout" id="layout2" v-model="layout" value="1">
           <label for="layout2">
-            <img src="../../../assets/flyer_layout_preview/Flyer_Layout_2_Vorschau.png" class="w-50">
+            <img src="../../../assets/flyer/Flyer-Layout-2-Vorschau.png" class="w-75">
           </label>
         </div>
 
         <div class="col">
           <input type="radio" class="sr-only" name="layout" id="layout3" v-model="layout" value="2">
           <label for="layout3">
-            <img src="../../../assets/flyer_layout_preview/Flyer-Layout-1-Vorschau-Color.png" class="w-50">
+            <img src="../../../assets/flyer/Flyer-Layout-3-Vorschau.png" class="w-75">
           </label>
         </div>
 
@@ -95,31 +95,53 @@ export default {
       this.img.src = window.URL.createObjectURL(event.target.files[0])
       this.base64 = await this.toBase64(event.target.files[0])
     },
+    processLocalFile(src) {
+      let image = new Image()
+      image.src = src
+      let canvas, ctx, dataURL, base64;
+      canvas = document.createElement("canvas");
+      ctx = canvas.getContext("2d");
+      canvas.width = image.width;
+      canvas.height = image.height;
+      console.log(image)
+      ctx.drawImage(image, 0, 0);
+      dataURL = canvas.toDataURL("image/png");
+      console.log(dataURL)
+      base64 = dataURL.replace(/^data:image\/png;base64,/, "");
+      console.log(base64)
+      return base64;
+    }
+    ,
 
-    createPDF: function () {
+    async createPDF() {
       let documentName = 'Flyer'
-      let doc = new JsPDF('p', 'mm', [297, 210]);
+      let doc = new JsPDF('p', 'mm', [297, 210])
       let qr = qrcode(4, 'L')
       qr.addData(this.qrCodeURL)
       qr.make()
+      doc.setFillColor(this.color)
+      doc.rect(0, 0, 210, 297, 'F')
+
+      doc.addImage(this.base64, 46.2, 17.3, 117.6, ((117.6 / this.img.width) * this.img.height))
+      doc.text(this.text, 11.4, 83.9)
 
       if (this.layout == 0) {
-        doc.setFillColor(this.color)
-        doc.rect(0, 0, 210, 297, 'F')
-
-        doc.addImage(this.base64, 46.2, 17.3, 117.6, ((117.6/this.img.width)*this.img.height))
-
-
-        //Element wird hier eingesetzt
-        doc.addImage(this.base64, 0, 202.6, 210, 94.4)
-
-
+        //doc.addImage(this.processLocalFile("../../assets/flyer/Flyer-Layout-1-Element.png"), 0, 202.6, 210, 94.4)
         doc.addImage(qr.createDataURL(), 27.7, 223.5, 54.8, 54.8)
-        doc.text(this.text, 11.4, 83.9)
-
         doc.save(documentName + '.pdf')
       }
 
+      if (this.layout == 1) {
+        //doc.addImage(this.processLocalFile("../../assets/flyer/Flyer-Layout-1-Element.png"), 0, 202.6, 210, 94.4)
+        doc.addImage(qr.createDataURL(), 27.7, 223.5, 54.8, 54.8)
+        doc.save(documentName + '.pdf')
+      }
+
+      if (this.layout == 2) {
+        //doc.addImage(this.processLocalFile("../../assets/flyer/Flyer-Layout-1-Element.png"), 0, 202.6, 210, 94.4)
+        doc.addImage(qr.createDataURL(), 27.7, 223.5, 54.8, 54.8)
+        doc.save(documentName + '.pdf')
+      }
     }
   }
 }
