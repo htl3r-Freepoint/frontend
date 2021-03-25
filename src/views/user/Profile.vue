17433fb1d72fe5d8083c8b62a8a8dd7f7b6d54ad
Mario Seidl <mario.s-@live.de>
2021-03-25 22:58:02 +0100

    Account löschen

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -26,7 +26,7 @@
       </fp-input>
     </form>
 
-    <button class="btn btn-primary" @click="changeUser">Speichern</button>
+    <button class="btn btn-primary" type="button" @click="changeUser">Speichern</button>
 
     <form>
       <fp-input title="Altes Passwort">
@@ -43,12 +43,32 @@
                placeholder="Neues Passwort"
                autocomplete="password">
       </fp-input>
+      <button class="btn btn-primary" type="button" @click="changePassword">Passwort Ändern</button>
     </form>
+
+    <button type="button" class="btn btn-danger font-weight-bold mt-3" @click="modalDelete">Account Löschen</button>
+
+    <transition name="fade" v-on:enter="enterSuccess">
+      <div class="alert alert-success" v-if="success">
+        Daten wurden gespeichert
+      </div>
+    </transition>
+
+    <modal id="deleteConfirmation">
+      <form class="d-flex flex-column">
+        <fp-input label="Geben sie ihr Passwort zur Bestätiugung ein">
+          <input class="form-control" type="password" v-model="userPassword">
+        </fp-input>
+        <button class="btn btn-danger" type="button" @click="deleteUser" :disabled="!userPassword">Löschen</button>
+      </form>
+    </modal>
   </div>
 </template>
 
 <script>
-import FpInput from "../../components/Form Components/FpInput";
+import FpInput from "@/components/Form Components/FpInput.vue";
+import Modal from "@/components/Modal.vue"
+
 import {library} from "@fortawesome/fontawesome-svg-core";
 import {faAt, faKey, faUser} from "@fortawesome/free-solid-svg-icons";
 
@@ -56,15 +76,17 @@ library.add(faUser, faAt, faKey)
 
 export default {
   name: "Profile",
-  components: {FpInput},
+  components: {FpInput, Modal},
   data() {
     return {
+      userPassword: '',
       username: '',
       firstName: '',
       lastName: '',
       email: '',
       oldPassword: '',
-      newPassword: ''
+      newPassword: '',
+      success: false
     }
   },
   created() {
@@ -79,6 +101,9 @@ export default {
     })
   },
   methods: {
+    modalDelete() {
+      this.$('#deleteConfirmation').modal()
+    },
     changeUser() {
       this.$http.post(this.$store.state.url + "/changeUser", {
         token: this.$store.state.user.token,
@@ -86,6 +111,9 @@ export default {
         firstName: this.firstName,
         lastName: this.lastName,
         email: this.email
+      }).then(response => {
+        response
+        this.success = true
       })
     },
     changePassword() {
@@ -93,12 +121,35 @@ export default {
         token: this.$store.state.user.token,
         oldPassword: this.oldPassword,
         newPassword: this.newPassword
+      }).then(response => {
+        response
+        this.success = true
       })
+    },
+    deleteUser(){
+      this.$http.post(this.$store.state.url + "/deleteUser", {
+        hash: this.$store.state.user.token,
+        password: this.userPassword
+      }).then(response => {
+        console.debug(response)
+        this.$('#deleteConfirmation').modal()
+      })
+    },
+    enterSuccess() {
+      setTimeout(() => {
+        this.success = false;
+      }, 3000);
     }
   }
 }
 </script>
 
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s
+}
 
+.fade-enter, .fade-leave-to {
+  opacity: 0
+}
 </style>
